5b045ae5e308e9269b81b87fbe8a6c96445234b0
zerobugofc00 <priyanshuu0386@gmail.com>
2026-09-13 17:05:58 +0530

    Add files via upload

---
+++ day4ques8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
